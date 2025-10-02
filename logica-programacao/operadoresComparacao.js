453f13db73322c603e2c7b817a0c6ab93243393f
Luis Martini <agenciamartinidigital@gmail.com>
2025-10-02 13:32:42 -0300

    Add

---
+++ logica-programacao/operadoresComparacao.js
@@ -0,0 +1,10 @@
+/**
+ * > maior
+ * < menor
+ * >= maior que
+ * <= menor que
+ * == igualdade (valor) não é recomendável
+ * === igualdade estrita (valor e tipo)
+ * != diferente
+ * !== diferente estrito
+ */
