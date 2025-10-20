dc8130d40b6238a631d8ce3d07dfcfadd79275ec
agenciamartinidigital <agenciamartinidigital@gmail.com>
2025-10-20 13:30:33 -0300

    att

--- Aula18 - funções/funcaoRecursiva.js
+++ Aula18 - funções/funcaoRecursiva.js
@@ -7,11 +7,30 @@
 // }
 // recursiva(0);
 
-function fibonnaci(n){
-    if (n < 2){ // caso-base
-        return n;
-    }
-    return fibonnaci(n - 1) + fibonnaci(n - 2); // recursividade
-}
-console.log(fibonnaci(20));
+// function fibonnaci(n){
+//     if (n < 2){ // caso-base
+//         return n;
+//     }
+//     return fibonnaci(n - 1) + fibonnaci(n - 2); // recursividade
+// }
+// console.log(fibonnaci(20));
 
+// function contagem(n) {
+//     if(n === 0) {
+//         console.log('Fim!');
+//         return;
+//     }
+//     console.log(n);
+//     contagem(n - 1); //caso recursivo
+// }
+// contagem(5);
+
+function fatorial(n){
+    if(n < 2) return n; // caso-base
+    return n * fatorial(n - 1); // caso recursivo
+}
+console.log(fatorial(3));
+
+/**
+ * n x (n-1)
+ */
