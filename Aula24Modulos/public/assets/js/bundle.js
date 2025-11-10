2d4f30cf07268a1ace94f4dd319f12c78260ad6a
agenciamartinidigital-sy <agenciamartinidigital@gmail.com>
2025-11-10 07:56:17 -0300

    Att

---
+++ Aula24Modulos/public/assets/js/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Eiiiiiiiiiiiiiiiiiiii');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
