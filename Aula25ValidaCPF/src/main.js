6a7379751c1541133d6a625201e0b914a4e325b4
agenciamartinidigital-sy <agenciamartinidigital@gmail.com>
2025-11-11 13:17:09 -0300

    Att

--- Aula25ValidaCPF/src/main.js
+++ Aula25ValidaCPF/src/main.js
@@ -1,1 +1,9 @@
+import GeraCPF from './modules/geraCPF.js';
 
+import './assets/css/style.css';
+
+(function(){
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCpf();
+})();
