50c909bdb71c6a35e98f9d70f730320b9553d978
agenciamartinidigital-sy <agenciamartinidigital@gmail.com>
2025-11-24 08:24:17 -0300

    Att

--- node/aula13-servidor/src/controllers/homeController.js
+++ node/aula13-servidor/src/controllers/homeController.js
@@ -3,7 +3,10 @@ exports.paginaInicial = (req, res) => {
   // req.flash("info", "Olá, Mundo!");
   // req.flash("error", "blablabla");
   // req.flash("success", "Opa");
-  res.render("index");
+  res.render("index", {
+    titulo: "Título da página",
+    numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  });
   return;
 };
 
