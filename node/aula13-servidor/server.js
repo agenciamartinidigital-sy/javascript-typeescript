bb486ba09278b599505f7fe5793eb706f6a5b6b2
agenciamartinidigital-sy <agenciamartinidigital@gmail.com>
2025-11-25 08:23:09 -0300

    Att

--- node/aula13-servidor/server.js
+++ node/aula13-servidor/server.js
@@ -23,6 +23,7 @@ const {
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
 
 const sessionOptions = session({
