0159e9058049715eb35330f5dd1cf3d5d5c28dd1
agenciamartinidigital-sy <agenciamartinidigital@gmail.com>
2025-12-04 05:53:42 -0300

    Att

--- agenda/src/middlewares/middleware.js
+++ agenda/src/middlewares/middleware.js
@@ -1,8 +1,8 @@
 // src/middlewares/middleware.js
 
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.errors = req.flash('errors');
-  res.locals.success = req.flash('success');
+  res.locals.errors = req.flash("errors");
+  res.locals.success = req.flash("success");
   res.locals.user = req.session.user;
   next();
 };
@@ -19,5 +19,13 @@ exports.csrfMiddleware = (req, res, next) => {
   next();
 };
 
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash("errors", "Você precisa fazer o login");
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+  next();
+};
 
 // module.exports = { middlewareGlobal, checkCSRFError, csrfMiddleware };
